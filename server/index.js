2a3751e5d2b67d0e2db2d5c0cb0fc7669baff6ae
Mohammed Nihal <mohammednihal691@gmail.com>
2024-01-27 13:19:52 +0530

    change

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import userRoute from "./Routes/userRoutes.js"
 import taskRoute from "./Routes/taskRoute.js"
 import cors from "cors"
+import AWS from 'aws-sdk'
 
 
 const app = express()
@@ -12,6 +13,11 @@ dotenv.config()
 app.use(express.json())
 app.use(cors())
 
+AWS.config.update({
+    region: 'your-dynamodb-region',
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  });
 mongoose.connect(process.env.MONGO_DB).then((res)=>{
     console.log("MongoDb connected");
 
